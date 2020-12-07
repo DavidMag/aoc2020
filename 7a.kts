eb81ed2826fe8e224a2aacf52294df60823012fb
David Magnusson <david_magnusson@icloud.com>
2020-12-07 07:04:12 +0100

    Day 7

---
+++ 7a.kts
@@ -0,0 +1,44 @@
+import java.io.File
+import kotlin.system.measureTimeMillis
+
+val millis = measureTimeMillis {
+    var rules = mutableMapOf<String, Map<String, Int>>()
+    File("./7.txt")
+            .readLines()
+            .forEach {
+                val split = it.split("bags contain")
+                var subRules = mutableMapOf<String, Int>()
+                split[1]
+                        .split(",")
+                        .forEach {
+                            val n = Character.getNumericValue(it.trim().first())
+                            val s = it
+                                    .replace("\\d|bag(s.|s|.|)".toRegex(), "")
+                                    .trim()
+                            subRules[s] = n
+                        }
+                rules[split[0].trim()] = subRules
+            }
+
+    var count = 0
+    rules.keys.forEach {
+        if (find("shiny gold", it, rules)) count++
+    }
+    println(count)
+}
+
+fun find(color: String, inColor: String, rules: Map<String, Map<String, Int>>): Boolean {
+    val rule = rules[inColor]
+    if (rule?.contains(color) == true) {
+        return true
+    } else {
+        rule?.forEach {
+            if (find(color, it.key, rules)) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+println("took: ${millis}ms")
---
+++ 7b.kts
@@ -0,0 +1,37 @@
+import java.io.File
+import kotlin.system.measureTimeMillis
+
+val millis = measureTimeMillis {
+    var rules = mutableMapOf<String, Map<String, Int>>()
+    File("./7.txt")
+            .readLines()
+            .forEach {
+                val split = it.split("bags contain")
+                var subRules = mutableMapOf<String, Int>()
+                split[1]
+                        .split(",")
+                        .forEach {
+                            val n = Character.getNumericValue(it.trim().first())
+                            val s = it
+                                    .replace("\\d|bag(s.|s|.|)".toRegex(), "")
+                                    .trim()
+                            if (s != "no other") {
+                                subRules[s] = n
+                            }
+                        }
+                rules[split[0].trim()] = subRules
+            }
+
+    println(countBags("shiny gold", rules) - 1)
+}
+
+fun countBags(color: String, rules: Map<String, Map<String, Int>>): Int {
+    var counter = 1
+    rules[color]?.forEach {
+        counter += it.value * countBags(it.key, rules)
+    }
+
+    return counter
+}
+
+println("took: ${millis}ms")
